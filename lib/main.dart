93d75380be5e6844b7218aeb29f6dd19758e9d8a
DAN <macabenlar.d@gmail.com>
2024-08-24 10:48:15 +0800

    Pogi Updated 08/24/24

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'firebase_options.dart';
 import 'package:test_again/Screens/log_in_page.dart';
-import 'package:test_again/student/student_home_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -54,12 +54,13 @@ class MyHomePage extends StatelessWidget {
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   Text(
-                    "Welcome to CISC KIDS APP!!",
+                    "Welcome to CISC KIDS APP!!  Iag",
                     textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontSize: 25.0,
-                      fontWeight: FontWeight.w900,
-                    ),
+                    style:  GoogleFonts.lexend(
+                          fontWeight: FontWeight.w500,
+                          fontSize: 25,
+                        ),
+                    
                   ),
                   SizedBox(height: 10),
                   Text(
@@ -71,37 +72,46 @@ class MyHomePage extends StatelessWidget {
                   ),
                 ],
               ),
-              const SizedBox(
-                width: 280,
-                child: Center(
-                  child: Text(
-                    "we believe in the trans-formative power of education. Our mission is to empower learners and educators alike by providing innovative assessment solutions that foster meaningful learning experiences. We understand that assessment is not just about testing; it's about understanding, growth, and continuous improvement",
-                    textAlign: TextAlign.justify,
-                  ),
-                ),
-              ),
-              Container(
-                height: 50,
-                width: 300,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(50),
-                  color: const Color(0xFF15A323),
-                ),
-                child: TextButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => const SignIn(),
+               Expanded(
+                 child: Container(
+                  padding: EdgeInsets.all(25),
+                  width: double.infinity,
+                  child: Center(
+                    child: Text(
+                      "we believe in the trans-formative power of education. Our mission is to empower learners and educators alike by providing innovative assessment solutions that foster meaningful learning experiences. We understand that assessment is not just about testing; it's about understanding, growth, and continuous improvement",
+                      textAlign: TextAlign.justify,
+                      style: GoogleFonts.lexend(
+                        fontWeight: FontWeight.w300,
+                        fontSize: 25,
                       ),
-                    );
-                  },
-                  child: const Text(
-                    "GET STARTED",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 25,
-                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                               ),
+               ),
+              Expanded(
+                child: Container(
+                  height: 50,
+                  width: 300,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(50),
+                    color: const Color(0xFF15A323),
+                  ),
+                  child: TextButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const SignIn(),
+                        ),
+                      );
+                    },
+                    child: const Text(
+                      "GET STARTED",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 25,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
                   ),
                 ),
